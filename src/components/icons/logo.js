529b24bf6be105573517977e53a10912ad1c3f1d
Harshal Raverkar <harshal.raverkar@gmail.com>
2019-05-27 21:56:09 +0530

    SVG icon changes.

--- src/components/icons/logo.js
+++ src/components/icons/logo.js
@@ -4,12 +4,7 @@ const IconLogo = () => (
   <svg xmlns="http://www.w3.org/2000/svg" role="img" viewBox="0 0 84 96">
   <title>Logo</title>
   <g id="Logo" transform="translate(4.000000, 1.000000)">
-  <g transform="translate(21.000000, 9.000000)">
-      <path
-        d="M4.39 59.70L4.39 9.28L10.16 9.28L10.16 30.73L25.84 30.73L25.84 9.28L31.61 9.28L31.61 59.70L25.84 59.70L25.84 36.21L10.16 36.21L10.16 59.70L4.39 59.70Z"
-        fill="#64FFDA"
-      />
-    </g>
+    
       <polygon
         id="Shape"
         stroke="#64FFDA"
@@ -18,6 +13,12 @@ const IconLogo = () => (
         strokeLinejoin="round"
         points="39 0 0 22 0 67 39 90 78 68 78 23"
       />
+      <g transform="translate(21.000000, 9.000000)">
+      <path
+        d="M4.39 59.70L4.39 9.28L10.16 9.28L10.16 30.73L25.84 30.73L25.84 9.28L31.61 9.28L31.61 59.70L25.84 59.70L25.84 36.21L10.16 36.21L10.16 59.70L4.39 59.70Z"
+        fill="#64FFDA"
+      />
+    </g>
   </g>
 </svg>
 );
